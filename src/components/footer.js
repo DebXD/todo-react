07ffdba5dedfd7e24d89339290deba047a69d44f
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-02-05 10:41:30 +0530

    add copyright footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <></>;
+  let footerStyle = {
+    position : "absolute",
+    top : "90vh",
+    width: "100%"
+  }
+  return (<footer className="bg-dark text-light py-3" style={footerStyle}>
+    <p className="text-center">
+    Copyright &copy; MyTodoList.com
+    </p>
+    
+  </footer>)
 };
 
 export default Footer;

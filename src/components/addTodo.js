220b32e66b28afb2e5484ccdc7a637eb0470d995
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-02-10 13:09:25 +0530

    feat: add todo via bootstrap modal

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -9,14 +9,54 @@ export default function AddTodo(props) {
         if (!title || !desc){
             alert('Title or description can not be empty!')
         }
-        props.addTODO(title, desc)
+        else{
+            props.addTODO(title, desc)
         setTitle("");
         setDesc("");
+        
+        }
         
     }
   return (
     <div className="container">
-    <form onSubmit={submit}>
+        {/* <!-- Button trigger modal --> */}
+<div className="mt-3">
+<button type="button" className="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  ADD
+</button>
+</div>
+
+{/* <!-- Modal --> */}
+<div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="staticBackdropLabel">ADD TODO</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+      <form onSubmit={submit}>
+          <div className="form-group">
+            <label className="col-form-label" >TITLE</label>
+            <input type="text" className="form-control"  value={title} onChange={e => setTitle(e.target.value)}/>
+          </div>
+          <div className="form-group">
+            <label  className="col-form-label">DESCRIPTION</label>
+            <textarea className="form-control" id="message-text" rows="3" value={desc} onChange={e => setDesc(e.target.value)}></textarea>
+          </div>
+        
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button className="btn btn-outline-dark btn-md my-3" data-bs-dismiss="modal">Submit</button>
+      </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    {/* <form onSubmit={submit}>
         
         <div className="mb-3 mt-3">
             <label>
@@ -42,7 +82,7 @@ export default function AddTodo(props) {
         <div className="text-center">
             <button className="btn btn-outline-dark btn-md my-3">Submit</button>
         </div>
-        </form>
+        </form> */}
         </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  let footerStyle = {
-    "font-size": "0.7rem",
-  };
-  return (<footer className="bg-dark text-light py-3">
-    <p className="text-center" style={footerStyle}>
-    Copyright &copy; MyTodoList.com
-    </p>
-    
-  </footer>)
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">Copyright &copy; MyTodoList.com</p>
+    </footer>
+  );
 };
 
 export default Footer;
